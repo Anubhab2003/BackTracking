65b62de8441557c4d747ae7454e25465833d9a33
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2023-08-22 14:07:57 +0000

    Important NQueen question

---
+++ NQueens.java
@@ -0,0 +1,62 @@
+public class NQueens {
+    public static void nqueen(char board[][], int row) {
+        // Base Case
+        if (row == board.length) {
+            printboard(board);
+            return;
+        }
+
+        // Recursion step column loop
+        for (int j = 0; j < board.length; j++) {
+            if (issafe(board, row, j)) {
+                board[row][j] = 'Q';
+                nqueen(board, row + 1); // Recursive step
+                board[row][j] = 'X'; // Backtracking step
+            }
+        }
+    }
+
+    public static void printboard(char board[][]) {
+        System.out.println("________________Chess Board____________");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static boolean issafe(char board[][], int row, int col) {
+        // Vertical up
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // Diagonal left up
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // Diagonal right up
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]) {
+        int n = 4;
+        char board[][] = new char[n][n];
+        // Initialize
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = 'X';
+            }
+        }
+        nqueen(board, 0);
+    }
+}
